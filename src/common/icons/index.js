3ef921a52cec24a2ae8ad7ec2c1f8f49b7115c31
Dico <ha3158987@gmail.com>
2021-07-21 18:18:23 +0900

    add: 아이콘 추가

---
+++ src/common/icons/index.js
@@ -0,0 +1,18 @@
+import { ReactComponent as WantedIcon } from './wanted-logo.svg';
+import { ReactComponent as BellIcon } from './bell.svg';
+import { ReactComponent as MagnifyingGlassIcon } from './magnifying-glass.svg';
+import { ReactComponent as RightArrowIcon } from './right-arrow.svg';
+import { ReactComponent as HamburgerIcon } from './hamburger.svg';
+
+const Icon = {
+  Wanted: WantedIcon, 
+  Bell: BellIcon, 
+  MagnifyingGlass: MagnifyingGlassIcon,
+  RightArrow: RightArrowIcon,
+  Hamburger:HamburgerIcon
+};
+
+export {
+  WantedIcon, BellIcon, MagnifyingGlassIcon, RightArrowIcon, HamburgerIcon
+};
+export default Icon;
